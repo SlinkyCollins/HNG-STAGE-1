32ccfa73d1938e3aabaaebf240e3780c5c05f4cb
Ademola Afolabi <afolabiademola27@gmail.com>
2024-07-02 11:03:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function updateTime() {
+        const now = new Date();
+        const utcTime = now.toUTCString().slice(-12, -4);
+        document.querySelector('[data-testid="currentTimeUTC"]').textContent = utcTime;
+    }
+
+    function updateDay() {
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const now = new Date();
+        const currentDay = days[now.getUTCDay()];
+        document.querySelector('[data-testid="currentDay"]').textContent = currentDay;
+    }
+
+    updateTime();
+    updateDay();
+    setInterval(updateTime, 1000); // Update time every second
+});
